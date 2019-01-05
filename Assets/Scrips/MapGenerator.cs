49bfca3e76546e7ed3ec47eb0b7de8234365b940
Matěj Moravec <moravec.matej94@gmail.com>
2019-01-05 16:18:55 +0100

    InstancedIndirect

--- Assets/Scrips/MapGenerator.cs
+++ Assets/Scrips/MapGenerator.cs
@@ -11,20 +11,31 @@ public class MapGenerator : MonoBehaviour {
     public Transform viewer;
     public Texture2D mapTexture;
 
-    int chunkSize = 254;
-
     private float oldScale;
 
     public int size;
 
     public Material material;
 
-    Dictionary<Vector2, PreObjects> terrainChunkDictionary = new Dictionary<Vector2, PreObjects>();
+    Dictionary<Vector2, TerrainChunk> terrainChunkDictionary = new Dictionary<Vector2, TerrainChunk>();
     Dictionary<float, PreObjects> preTerrainChunkDictionary = new Dictionary<float, PreObjects>();
+    Dictionary<float, PreMesh> PreMeshDictionary = new Dictionary<float, PreMesh>();
+
+
+    public Material instanceMaterial;
+    public int subMeshIndex = 0;
+
+
+    private int cachedInstanceCount = -1;
+    private int cachedSubMeshIndex = -1;
+    private ComputeBuffer positionBuffer;
+    private ComputeBuffer argsBuffer;
+    private uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
 
     void Start()
     {
         oldScale = scale;
+        CreatePreMesh();
         UpdateChunkMesh();
     }
 
@@ -37,106 +48,124 @@ public class MapGenerator : MonoBehaviour {
         }
     }
 
-    public void UpdateChunkMesh()
+    void CreatePreMesh()
     {
+        float relativeScale = 0.5f;
+
+        while (relativeScale < 10)
+        {
+            PreMeshDictionary.Add(relativeScale, new PreMesh(relativeScale));
+            relativeScale += relativeScale;
+        }
+    }
+
+    void UpdateChunkMesh()
+    {
+        Debug.Log("Hello");
         viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
-        PreObjects clone;
 
         int currentChunkCoordX = Mathf.RoundToInt(0);
         int currentChunkCoordY = Mathf.RoundToInt(0);
 
-        //DeleteObject();
+        int instanceCount = (chunksVisibleInViewDst * 2 + 1) * (chunksVisibleInViewDst * 2 + 1);
 
-        float relativeScale = 0.5f;
-
-        while(relativeScale < 10){
-            preTerrainChunkDictionary.Add(relativeScale, new PreObjects(material, relativeScale, mapTexture));
-            relativeScale += relativeScale;
-        }
+        Vector4[] viewedChunkCoord = new Vector4[instanceCount];
+        int positionIndex = 0;
 
         for (int yOffset = -chunksVisibleInViewDst; yOffset <= chunksVisibleInViewDst; yOffset++)
         {
             for (int xOffset = -chunksVisibleInViewDst; xOffset <= chunksVisibleInViewDst; xOffset++)
             {
-                Vector2 viewedChunkCoord = new Vector2(currentChunkCoordX + xOffset, currentChunkCoordY + yOffset) * (size - 1);
-                Debug.Log(viewedChunkCoord);
-                clone = Instantiate(preTerrainChunkDictionary[1] , new Vector3(viewedChunkCoord.x, 0, viewedChunkCoord.y), Quaternion.identity);
-
-                terrainChunkDictionary.Add(viewedChunkCoord, clone);
-
+                Debug.Log(positionIndex);
+                viewedChunkCoord[positionIndex] = new Vector4(currentChunkCoordX + xOffset, 0,currentChunkCoordY + yOffset, 1) * (size - 1);
+                positionIndex++;
             }
         }
+        UpdateBuffers(viewedChunkCoord, instanceCount, PreMeshDictionary[1].GetMesh());
+        Debug.Log("Hello");
+        Graphics.DrawMeshInstancedIndirect(PreMeshDictionary[1].GetMesh(), subMeshIndex, instanceMaterial, new Bounds(Vector3.zero, new Vector3(100.0f, 100.0f, 100.0f)), argsBuffer);
     }
 
-
-    void DeleteObject()
+    void UpdateBuffers(Vector4[] positions, int instanceCount, Mesh instanceMesh)
     {
-        foreach (var entry in terrainChunkDictionary)
-        {
-           // entry.Value.DeleteObject();        
-       //     terrainChunkDictionary.Remove(entry.Key);
-        }
-    }
+        // Ensure submesh index is in range
+        if (instanceMesh != null)
+            subMeshIndex = Mathf.Clamp(subMeshIndex, 0, instanceMesh.subMeshCount - 1);
 
+        // Positions
+        if (positionBuffer != null)
+            positionBuffer.Release();
+        positionBuffer = new ComputeBuffer(instanceCount, 16);
+
+        positionBuffer.SetData(positions);
+        instanceMaterial.SetBuffer("positionBuffer", positionBuffer);
+
+        // Indirect args
+        if (instanceMesh != null)
+        {
+            args[0] = (uint)instanceMesh.GetIndexCount(subMeshIndex);
+            args[1] = (uint)instanceCount;
+            args[2] = (uint)instanceMesh.GetIndexStart(subMeshIndex);
+            args[3] = (uint)instanceMesh.GetBaseVertex(subMeshIndex);
+        }
+        else
+        {
+            args[0] = args[1] = args[2] = args[3] = 0;
+        }
+        argsBuffer.SetData(args);
+
+        cachedInstanceCount = instanceCount;
+        cachedSubMeshIndex = subMeshIndex;
+    }
 
     public class TerrainChunk
     {
         GameObject meshObject;
-        Vector2 position;
-        Vector3 position3d;
+        Vector3 position;
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
         MeshCollider meshCollider;
+
+        float scale;
        
 
-        public TerrainChunk(Material material, Vector2 coord, int size, float scale,Texture2D mapTexture)
+        public TerrainChunk(GameObject meshObject, Vector2 coord, float scale)
         {
+            this.meshObject = meshObject;
+            this.scale = scale;
 
-            position = coord * (size - 1);
-
-            Vector3 positionV3 = new Vector3(position.x, 0, position.y);       
-
-            meshObject = new GameObject("Terrain Chunk");
             meshRenderer = meshObject.AddComponent<MeshRenderer>();
             meshFilter = meshObject.AddComponent<MeshFilter>();
             meshCollider = meshObject.AddComponent<MeshCollider>();
-            meshRenderer.material = material;
-
-            meshRenderer.material.mainTexture = mapTexture;
-
-            meshObject.transform.position = positionV3 * scale;        
-            meshObject.transform.localScale = Vector3.one ;
-
-
-            meshFilter.mesh = MeshGenerator.GenerateTerrainMesh(scale).CreateMesh();
-        }
-
-        public void DeleteObject()
-        {
-            Destroy(meshObject);
-        }
-
-        public void NewTerrainCHunk(Material material, Vector2 coord, int size, float scale, Texture2D mapTexture)
-        {
-            position = coord * (size - 1);
-
-            Vector3 positionV3 = new Vector3(position.x, 0, position.y);
-
-            meshObject = new GameObject("Terrain Chunk");
-            meshRenderer = meshObject.AddComponent<MeshRenderer>();
-            meshFilter = meshObject.AddComponent<MeshFilter>();
-            meshCollider = meshObject.AddComponent<MeshCollider>();
-            meshRenderer.material = material;
-
-            meshRenderer.material.mainTexture = mapTexture;
-
-            meshObject.transform.position = positionV3 * scale;
+            position = meshObject.transform.position;
             meshObject.transform.localScale = Vector3.one;
-
-
-            meshFilter.mesh = MeshGenerator.GenerateTerrainMesh(scale).CreateMesh();
+            SetVisible(true);
         }
+
+        public void SetVisible(bool visible)
+        {
+            meshObject.SetActive(visible);
+
+        }
+    }
+
+    public class PreMesh
+    {
+        Mesh preMesh;
+        float scale;
+
+        public PreMesh(float scale)
+        {
+            this.scale = scale;
+            preMesh = MeshGenerator.GenerateTerrainMesh(scale).CreateMesh();
+        }
+
+        public Mesh GetMesh()
+        {
+            return preMesh;
+        }
+        
     }
 
     public class PreObjects
@@ -144,8 +173,6 @@ public class MapGenerator : MonoBehaviour {
         GameObject meshObject;
         Vector2 position;
         Vector3 position3d;
-
-        Bounds bounds;
 
         MeshRenderer meshRenderer;
         MeshFilter meshFilter;
--- Assets/Shaders/Terrain.shader
+++ Assets/Shaders/Terrain.shader
@@ -10,12 +10,13 @@ Shader "Custom/Terrain" {
 	}
 		SubShader{
 			Tags { "RenderType" = "Opaque" }
-			LOD 20
+			LOD 200
 
 			CGPROGRAM
 			// Physically based Standard lighting model, and enable shadows on all light types
-			#pragma surface surf Standard fullforwardshadows vertex:vert
-
+			#pragma surface surf Standard addshadow fullforwardshadows vertex:vert
+			#pragma multi_compile_instancing
+			#pragma instancing_options procedural:setup
 
 			// Use shader model 3.0 target, to get nicer looking lighting
 			#pragma target 3.0
@@ -26,6 +27,35 @@ Shader "Custom/Terrain" {
 				float2 uv_MainTex;
 				float3 worldPos;
 			};
+
+			#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+				StructuredBuffer<float4> positionBuffer;
+			#endif
+
+				void rotate2D(inout float2 v, float r)
+				{
+					float s, c;
+					sincos(r, s, c);
+					v = float2(v.x * c - v.y * s, v.x * s + v.y * c);
+				}
+
+				void setup()
+				{
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+					float4 data = positionBuffer[unity_InstanceID];
+
+					float rotation = data.w * data.w * _Time.y * 0.5f;
+					rotate2D(data.xz, rotation);
+
+					unity_ObjectToWorld._11_21_31_41 = float4(data.w, 0, 0, 0);
+					unity_ObjectToWorld._12_22_32_42 = float4(0, data.w, 0, 0);
+					unity_ObjectToWorld._13_23_33_43 = float4(0, 0, data.w, 0);
+					unity_ObjectToWorld._14_24_34_44 = float4(data.xyz, 1);
+					unity_WorldToObject = unity_ObjectToWorld;
+					unity_WorldToObject._14_24_34 *= -1;
+					unity_WorldToObject._11_22_33 = 1.0f / unity_WorldToObject._11_22_33;
+#endif
+				}
 
 			half _Glossiness;
 			half _Metallic;
@@ -49,7 +79,7 @@ Shader "Custom/Terrain" {
 				// Albedo comes from a texture tinted by color
 
 				float4 wolrldPosition = mul(unity_ObjectToWorld, float4(IN.worldPos, 0));
-				fixed4 c = tex2D(_MainTex, wolrldPosition.xz / 1000) ;
+				fixed4 c = tex2D(_MainTex, wolrldPosition.xz / 1000);
 				o.Albedo = c.rgb;
 
 				// Metallic and smoothness come from slider variables
